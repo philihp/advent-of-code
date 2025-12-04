190d18b25630c9d4271de69294506e3aaae61501
Philihp Busby <philihp@gmail.com>
2025-12-04 06:57:17 +0000

    2025 day 3 part 2 - fixes

--- src/2025/day03/part2.js
+++ src/2025/day03/part2.js
@@ -1,17 +1,18 @@
 import fs from 'fs'
-import { concat, pipe, map, split, slice, max, flatten, sum, range, filter } from 'ramda'
+import { pipe, map, split, slice, flatten, sum } from 'ramda'
 
 const data = fs.readFileSync('src/2025/day03/input2.txt', 'utf8')
 
 export const maxJoltWithBatteries = (n) => (s) => {
-  if (n === 1) {
-    return Math.max(...s)
-  }
-  const subArr = slice(0, s.length - 1, s)
+  if (n === 1) return `${Math.max(...s)}`
+  if (n < 0) return ''
+  if (s.length === 0) return ''
+  const subArr = slice(0, s.length - n + 1, s)
   const maxSub = Math.max(...subArr)
   const maxLoc = subArr.indexOf(maxSub)
   const aftMax = slice(maxLoc + 1, s.length, s)
-  return 10 * maxSub + maxJoltWithBatteries(n - 1)(aftMax)
+  const suffix = maxJoltWithBatteries(n - 1)(aftMax)
+  return `${maxSub}${suffix}`
 }
 
 pipe(
@@ -19,7 +20,7 @@ pipe(
   split('\n'),
   map(split('')),
   map(map((s) => Number.parseInt(s, 10))),
-  map(maxJoltWithBatteries(2)),
+  map((row) => Number.parseInt(maxJoltWithBatteries(12)(row), 10)),
   flatten,
   sum,
   console.log
--- src/2025/day03/part2.test.js
+++ src/2025/day03/part2.test.js
@@ -3,8 +3,32 @@ import { describe, it } from 'node:test'
 import assert from 'node:assert/strict'
 
 describe('maxJoltWithBatteries', () => {
+  it('can pick 1 out of an array', () => {
+    const actual = maxJoltWithBatteries(1)([9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1])
+    assert.equal(actual, '9')
+  })
   it('can pick 2 out of an array', () => {
     const actual = maxJoltWithBatteries(2)([9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1])
-    assert.equal(actual, 98)
+    assert.equal(actual, '98')
+  })
+  it('can pick 3 out of an array', () => {
+    const actual = maxJoltWithBatteries(3)([1, 9, 8, 7, 6])
+    assert.equal(actual, '987')
+  })
+  it('can pick first 4 out of an array', () => {
+    const actual = maxJoltWithBatteries(4)([9, 9, 8, 1, 1, 1, 1, 1, 1, 1])
+    assert.equal(actual, '9981')
+  })
+  it('can pick last 4 out of an array', () => {
+    const actual = maxJoltWithBatteries(4)([1, 1, 1, 1, 1, 1, 1, 9, 9, 9, 9])
+    assert.equal(actual, '9999')
+  })
+  it('can pick from edges', () => {
+    const actual = maxJoltWithBatteries(7)([9, 8, 7, 6, 5, 6, 7, 8, 9])
+    assert.equal(actual, '9876789')
+  })
+  it('can pick out from middle', () => {
+    const actual = maxJoltWithBatteries(7)([5, 6, 7, 8, 9, 8, 7, 6, 5])
+    assert.equal(actual, '7898765')
   })
 })
