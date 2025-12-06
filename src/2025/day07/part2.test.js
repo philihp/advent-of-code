e93374ab3c67d0959fcaac7a397c9aab04495551
Philihp Busby <philihp@gmail.com>
2025-12-06 07:51:11 +0000

    2025 day 7 boilerplate

---
+++ src/2025/day07/part2.test.js
@@ -0,0 +1,10 @@
+import {} from './part2.js'
+import { describe, it } from 'node:test'
+import assert from 'node:assert/strict'
+
+describe('boilerplate', () => {
+  it('runs tests', () => {
+    const actual = []
+    assert.deepEqual(actual, [])
+  })
+})
