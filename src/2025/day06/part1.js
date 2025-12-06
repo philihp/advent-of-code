c79fca93bb818c22cf57ce1dcc3688cc44a8a1f6
Philihp Busby <philihp@gmail.com>
2025-12-06 07:11:26 +0000

    2025 day 6 part 1

--- src/2025/day06/part1.js
+++ src/2025/day06/part1.js
@@ -1,5 +1,5 @@
 import fs from 'fs'
-import { head, reduce, complement, pipe, map, split, compose, flatten, sum, filter } from 'ramda'
+import { equals, reverse, transpose, complement, pipe, map, split, sum, product, filter } from 'ramda'
 
 const readData = (file) => fs.readFileSync(file, 'utf8')
 
@@ -9,6 +9,24 @@ pipe(
   //
   readData,
   split('\n'),
-  map(atoi),
+  map(
+    pipe(
+      //
+      split(/ +/),
+      filter(complement(equals('')))
+    )
+  ),
+  transpose,
+  map(reverse),
+  map(([operation, ...terms]) => [operation, ...map(atoi, terms)]),
+  map(([operation, ...terms]) => {
+    switch (operation) {
+      case '*':
+        return product(terms)
+      case '+':
+        return sum(terms)
+    }
+  }),
+  sum,
   console.log
 )('src/2025/day06/input1.txt')
