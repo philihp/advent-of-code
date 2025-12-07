bfdf56cdc8b26889b921670cceafd8080fc5739c
Philihp Busby <philihp@gmail.com>
2025-12-07 11:46:14 +0000

    2025 day 7 part 1

--- src/2025/day07/part1.js
+++ src/2025/day07/part1.js
@@ -1,11 +1,41 @@
 import fs from 'fs'
-import { pipe, split } from 'ramda'
+import { pipe, add, transpose, map, split, reduce, aperture, count } from 'ramda'
 
 const readData = (file) => fs.readFileSync(file, 'utf8')
+
+const isSplit = ([o, n]) => o === '|' && n === '^'
+
+const cardinal = (arr) => aperture(3, [undefined, ...arr, undefined])
+
+const splitField = ([splits, oldField], newField) =>
+  oldField === undefined
+    ? [splits, newField]
+    : [
+        pipe(
+          //
+          transpose,
+          count(isSplit),
+          add(splits)
+        )([oldField, newField]),
+        pipe(
+          map(cardinal),
+          transpose,
+          map(([[a, b, c], [d, e, f]]) => {
+            // this would have been cool with pattern matching
+            if (a === '|' && d === '^') return '|'
+            if (c === '|' && f === '^') return '|'
+            if (e === '^') return '^'
+            if (b === 'S') return '|'
+            if (b === '|') return '|'
+            return e
+          })
+        )([oldField, newField]),
+      ]
 
 pipe(
   //
   readData,
   split('\n'),
+  reduce(splitField, [0, undefined]),
   console.log
 )('src/2025/day07/input1.txt')
